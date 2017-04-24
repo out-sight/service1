9626a8e20a585d8fd3d1348de4968dd82d412ad0
Vineesh_Devasia <vinihi83@gmail.com>
2017-04-24 14:12:17 +0530

    Init a simple rest service (http://start.spring.io/ : web,config-client,actuator)
    AND 
    
    fetch a service specific property and use it.

---
+++ outsight-service1/src/main/java/com/outsight/service1/OutsightService1Application.java
@@ -0,0 +1,12 @@
+package com.outsight.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsightService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutsightService1Application.class, args);
+	}
+}
